c6dd4d39ae3af35c3e489776983372ecd587c511
scottjstewart <scottstewart965@gmail.com>
2019-01-16 14:57:29 -0500

    fixed issue and finalized rough draft

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema");
-
+const path = require("path");
 const app = express();
+require("dotenv").config();
+
+//Allow cross-origin
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -12,6 +17,12 @@ app.use(
   })
 );
 
-const PORT = 5001;
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
